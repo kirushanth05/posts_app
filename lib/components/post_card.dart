b22807163dc93b5df7559059d0630d5637d4e9ce
kirushanth05 <kirushanthvijay@gmail.com>
2025-02-23 22:35:18 +0530

    post card has been modified to show a bit of content.

--- lib/components/post_card.dart
+++ lib/components/post_card.dart
@@ -5,6 +5,7 @@ class PostCard extends StatelessWidget {
   const PostCard({
     super.key,
   });
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -15,40 +16,56 @@ class PostCard extends StatelessWidget {
         border: Border.all(color: Color(0xffEBEBEB), width: 2),
         borderRadius: BorderRadius.circular(8),
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        mainAxisSize: MainAxisSize.max,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // title and description
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
             children: [
-              Text(
-                'Technology',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
+              // title and description
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Technology',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    '14k Views',
+                    style: TextStyle(color: Colors.grey.shade600),
+                  ),
+                ],
+              ),
+
+              // show more button
+              IconButton(
+                onPressed: () {
+                  showDetails(context);
+                },
+                icon: Icon(
+                  Icons.keyboard_arrow_right_rounded,
                 ),
-              ),
-              Text(
-                '14k Followers',
-                style: TextStyle(color: Colors.grey.shade600),
-              ),
+              )
             ],
           ),
-          // show more button
-          IconButton(
-            onPressed: () {
-              showDetails(context);
-            },
-            icon: Icon(
-              Icons.keyboard_arrow_right_rounded,
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: Text(
+              'Dolore esse eu deserunt aliqua aliquip labore sunt. Reprehenderit ut dolor exercitation exercitation. Est dolore ut Lorem non ipsum ex quis laborum elit non laboris dolor. Consequat deserunt officia consectetur deserunt mollit laborum et nostrud cupidatat mollit aliqua enim consequat nisi enim. Sit ad amet incididunt cillum non incididunt adipisicing veniam duis consequat laboris laboris cillum qui veniam. Dolore minim voluptate velit laboris velit elit quis commodo irure cillum. Sit minim nulla dolor fugiat in quis. ',
+              style: TextStyle(color: Colors.grey.shade600),
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
             ),
-          )
+          ),
         ],
       ),
     );
   }
+
   void showDetails(BuildContext context) {
     Navigator.push(
       context,
