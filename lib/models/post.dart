c3757bcfa243f409facb9648cf34e7660576439d
kirushanth05 <kirushanthvijay@gmail.com>
2025-02-23 22:36:07 +0530

    a post model has been created as a blueprint for posts.

---
+++ lib/models/post.dart
@@ -0,0 +1,21 @@
+import 'dart:math';
+
+final random = Random();
+
+class Post {
+  String id;
+  String title;
+  String author;
+  String content;
+  DateTime createdAt;
+  int views;
+
+  Post({
+    required this.id,
+    required this.title,
+    required this.author,
+    required this.content,
+    required this.createdAt,
+    this.views = 1446,
+  });
+}
