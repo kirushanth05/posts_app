bc21de934667a4342761573589f04bfa7a294fdf
kirushanth05 <kirushanthvijay@gmail.com>
2025-02-26 23:43:55 +0530

    dotenv loaded in the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:posts_app/pages/home_screen.dart';
 
-void main() => runApp(
-      MaterialApp(
-        // debugShowCheckedModeBanner: false,
-        home: HomeScreen(),
-      ),
-    );
+void main() async {
+  await dotenv.load(fileName: ".env");
+  return runApp(
+    MaterialApp(
+      // debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
+}
