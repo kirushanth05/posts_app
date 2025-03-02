bed9f93e743f3d28cbd092c1ec08a60237c575e3
kirushanth05 <kirushanthvijay@gmail.com>
2025-03-02 22:50:19 +0530

    edit/delete post functionality added.

--- lib/pages/post_screen.dart
+++ lib/pages/post_screen.dart
@@ -2,24 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:posts_app/components/default_app_bar.dart';
 import 'package:posts_app/models/post.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:posts_app/pages/create_update_post_screen.dart';
+// import 'dart:convert';
 
-class PostScreen extends StatelessWidget {
-  const PostScreen({super.key, required this.post});
+class PostScreen extends StatefulWidget {
+  const PostScreen({super.key, required this.post, this.onDelete});
 
   final Post post;
+  final Function()? onDelete;
 
+  @override
+  State<PostScreen> createState() => _PostScreenState();
+}
+
+class _PostScreenState extends State<PostScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: defaultAppBar(context),
+      appBar: defaultAppBar(
+        context,
+        showActions: true,
+        onEdit: () {
+          _editPost(context);
+        },
+        onDelete: () {
+          _deletePost(context);
+        },
+      ),
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              post.title,
+              widget.post.title,
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             const Gap(8),
@@ -27,18 +46,46 @@ class PostScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  ('By ${post.author}').toUpperCase(),
+                  ('By ${widget.post.author}').toUpperCase(),
                   style: TextStyle(fontWeight: FontWeight.bold),
                 ),
                 Text(
-                    '${post.createdAt.year}/ ${post.createdAt.month} / ${post.createdAt.day}'),
+                    '${widget.post.createdAt.year}/ ${widget.post.createdAt.month} / ${widget.post.createdAt.day}'),
               ],
             ),
             const Gap(16),
             Text(
-              post.content,
+              widget.post.content,
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  _deletePost(BuildContext context) async {
+    Uri url = Uri.https(dotenv.env['FIREBASE_REALTIME_DB_URL']!,
+        'posts/${widget.post.id}.json');
+    final response = await http.delete(url);
+
+    if (response.statusCode == 200 && widget.onDelete != null)
+      widget.onDelete!();
+    Navigator.pop(context);
+  }
+
+  void _editPost(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => CreatePostScreen(
+          post: widget.post,
+          onUpdate: (updatedPost) {
+            setState(() {
+              widget.post.title = updatedPost.title;
+              widget.post.author = updatedPost.author;
+              widget.post.content = updatedPost.content;
+            });
+          },
         ),
       ),
     );
